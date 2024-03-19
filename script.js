2e5c364cd8c9f732c55335bd7a2ec115181de4c5
José Borges Costa <borgesdevfront@gmail.com>
2024-03-19 10:59:13 -0300

    up

--- script.js
+++ script.js
@@ -146,7 +146,7 @@ checkoutBtn.addEventListener("click", function() {
   }).join("")
 
   const message = encodeURIComponent(cartItems)
-  const phone = "94992147394"
+  const phone = ""
 
   window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_blank")
 
